4cd815369c523671edc9a77076ab165107aac8cd
DwarakaCherukuri <77140643+DwarakaCherukuri@users.noreply.github.com>
2021-07-05 21:04:30 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,9 @@
+print("Hello there! \n Do you want to know about me?")
+reply= input("Your reply, yes or no?")
+if (reply =="yes"or reply == "YES"):
+    print("Hey! I am Dwaraka. I love eating desserts, I love making new friends and I absolutely love \n helping people when they're trying to get good at something. However, I have a \n slightly high elevated sense of self and every time I sit down to code, it feels like my \n  computer is laughing at me. I badly want to change that. So, here I am, taking baby steps \n to get better at programming.")
+elif (reply=="no" or reply== "NO"):
+    print("Okay, have a great day!")
+else:
+    print("He he he")
+
